e754509d53196e2b72b13a2514ea84ddf07735b2
Ville M. Vainio <ville.ma.vainio@nokia.com>
2012-08-28 13:08:54 +0300

    add stuff

---
+++ WpWeddit/www/js/index.js
@@ -0,0 +1,31 @@
+var app = {
+    initialize: function() {
+        this.bind();
+        
+    },
+    bind: function() {
+        document.addEventListener('deviceready', this.deviceready, false);
+    },
+    deviceready: function() {
+        // note that this is an event handler so the scope is that of the event
+        // so we need to call app.report(), and not this.report()
+				
+
+                
+				console.log("Hello world!");
+                console.log(window.hello);
+				console.log(window.Store)
+        
+
+		
+		console.log("prestart");
+		console.log(window.redditapp);
+
+		window.redditapp.start();
+		console.log("poststart");
+		app.report('deviceready');
+    },
+    report: function(id) { 
+        console.log("report:" + id);
+    }
+};
